0d7ba92cfd4e09879d7e6779e8fe0915bc20a01f
DANIEL-ANDREI FLOREA <daniel.andrei.florea@stud.ubbcluj.ro>
2024-11-06 19:28:09 +0200

    Laborator5, pașii 1-12

--- Migrations/20241106171731_Borrowings.Designer.cs
+++ Migrations/20241106171731_Borrowings.Designer.cs
@@ -4,6 +4,7 @@ using Florea_Daniel_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Florea_Daniel_Lab2.Migrations
 {
     [DbContext(typeof(Florea_Daniel_Lab2Context))]
-    partial class Florea_Daniel_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20241106171731_Borrowings")]
+    partial class Borrowings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
