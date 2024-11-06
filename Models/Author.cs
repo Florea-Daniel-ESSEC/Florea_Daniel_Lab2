11c032d8fd16dbeed775bfb1e4457bf91ab7f2c6
DANIEL-ANDREI FLOREA <daniel.andrei.florea@stud.ubbcluj.ro>
2024-11-06 19:12:58 +0200

    Laborator 4

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,10 +1,20 @@
-﻿namespace Florea_Daniel_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Florea_Daniel_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; } //navigation property
     }
 }
--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Florea_Daniel_Lab2.Data;
 using Florea_Daniel_Lab2.Models;
+using Florea_Daniel_Lab2.Models.ViewModels;
 
 namespace Florea_Daniel_Lab2.Pages.Categories
 {
@@ -20,10 +21,28 @@ namespace Florea_Daniel_Lab2.Pages.Categories
         }
 
         public IList<Category> Category { get;set; } = default!;
+        public CategoryIndexData CategoryData { get; set; }
+        public int? CategoryID { get; set; }
+        public int BookID { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id, int? categoryID)
         {
-            Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+               .Include(c => c.BookCategories)
+                    .ThenInclude(bc => bc.Book)
+                    .ThenInclude(c => c.Author)
+                .OrderBy(i => i.CategoryName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                CategoryData.Books = CategoryData.Categories
+                    .Where(c => c.ID == CategoryID.Value)
+                    .SelectMany(c => c.BookCategories
+                    .Select(bc => bc.Book));
+            }
         }
     }
 }
